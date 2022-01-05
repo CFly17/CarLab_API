a1b7f4e3dc3fc13ed5715a6d819f651f60be78e5
Cullin Flynn <cullinflynn@rocketmortgage.com>
2022-01-05 16:34:37 -0500

    first commit with two search functions

---
+++ CarLab_API/CarLot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace CarLab_API
+{
+    public class CarLot
+    {
+        public List<Car> Inventory { get; set; } = new List<Car>();
+        public CarLot()
+            {
+            Inventory.Add(new Car("Ford", "F-150", 2021, "blue"));
+            Inventory.Add(new Car("Chevy", "Colorado", 2021, "red"));
+            Inventory.Add(new Car("Mercedes Benz", "Popemobile", 2021, "white"));
+            Inventory.Add(new Car("Ford", "F-150", 2001, "black"));
+            Inventory.Add(new Car("Chevy", "Colorado", 2012, "blue"));
+            Inventory.Add(new Car("Rolls Royce", "Ghost", 2018, "black"));
+
+        }
+    }
+}
---
+++ CarLab_API/Controllers/CarLotController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarLab_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class CarLotController : ControllerBase
+    {
+        CarLot inv = new CarLot();
+        //public List<Car> = new List<Car>
+        public List<Car> listOfCars { get; set; } = new List<Car>();
+        [HttpGet]
+        public List<Car> GetAllCars()
+        {
+            return inv.Inventory;
+        }
+
+        [HttpGet("GetCarByIndex/{index}")]
+        public Car GetCarByIndex(int index)
+        {
+            Car clownCar = new Car("Clown", "Premium", 2050, "rainbow");
+            try
+            {
+                return inv.Inventory[index];
+            }
+            catch 
+            {
+                return clownCar;
+            }
+        }
+
+        [HttpGet("SearchMake/{search}")]
+        public List<Car> SearchMake(string search)
+        {
+            List<Car> listOfCarsByMake = new List<Car>();
+            foreach(Car c in inv.Inventory)
+            {
+                if(search.ToLower().Trim() == c.Make.ToLower().Trim())
+                {
+                    listOfCarsByMake.Add(c);
+                }
+            }
+            return listOfCarsByMake;
+        }
+
+        [HttpGet("SearchColor/{search}")]
+        public List<Car> SearchColor(string search)
+        {
+            List<Car> listOfCarsByColor = new List<Car>();
+            foreach (Car c in inv.Inventory)
+            {
+                if (search.ToLower() == c.Color.ToLower())
+                {
+                    listOfCarsByColor.Add(c);
+                }
+            }
+            return listOfCarsByColor;
+        }
+
+    }
+}
